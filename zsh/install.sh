fab52897dbf2e388e6ee57f6fcd862c1c5380ac7
Florian Hye <florian@hye.dev>
2021-10-14 19:45:50 +0200

    added omz and custom zsh configs

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-echo "linking .zshrc"
-ln -s $(dirname "$(realpath $0)")/.zshrc ~/.zshrc
+echo "Linking .zshrc"
+ln -s $(dirname "$(realpath $0)")/.zshrc $HOME/.zshrc
+
+echo "Installing Oh-My-Zsh..."
+cd $HOME
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing powerlevel10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Linking .p10k.zsh"
+ln -s $(dirname "$(realpath $0)")/omz/.p10k.zsh $HOME/.p10k.zsh
+
+echo "Linking Oh-My-Zsh custom"
+ln -s $(dirname "$(realpath $0)")/omz/custom $HOME/.oh-my-zsh/custom
---
+++ zsh/omz/custom/aliases.zsh
@@ -0,0 +1,21 @@
+# go into the programming folder
+alias go.prg="cd ~/Syncthing/Programmieren"
+alias go.node="cd ~/Syncthing/Programmieren/Node"
+alias go.web="cd ~/Syncthing/Programmieren/Web"
+alias go.dpl="cd ~/Syncthing/Programmieren/Node/dpl_v4"
+
+# go into fhwn folders
+alias go.fhwnRoot="cd ~/Syncthing/Dokumente/Schule/FHWN/"
+alias go.fhwnS1="cd ~/Syncthing/Dokumente/Schule/FHWN/1.\ Semester\ W2020"
+alias go.fhwnS2="cd ~/Syncthing/Dokumente/Schule/FHWN/2.\ Semester\ S2021"
+alias go.fhwnS3="cd ~/Syncthing/Dokumente/Schule/FHWN/3.\ Semester\ W2021"
+alias go.fhwn="go.fhwnS3"
+
+# go into work dirs
+alias go.expanic="cd ~/Syncthing/Dokumente/Arbeit/expanic"
+
+
+# Find & Delete all ".DS_Store" files (recursive)
+alias delete.ds="find . -name '.DS_Store' -type f -print -delete"
+
+alias zsh.reload=". ~/.zshrc"
