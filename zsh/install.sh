2a2c558db75f3c06b290f60c85993052358d0aed
Florian Hye <florian@hye.dev>
2021-10-14 20:09:50 +0200

    zsh: linking .zshrc after omz install, added -f

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #$(dirname "$(realpath $0)")
-echo "Linking .zshrc"
-ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
-
 echo "Installing Oh-My-Zsh..."
 cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "Installing powerlevel10k theme..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Linking .zshrc"
+ln -sf $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 
 echo "Linking .p10k.zsh"
 ln -s $HOME/dotfiles/zsh/omz/.p10k.zsh $HOME/.p10k.zsh
