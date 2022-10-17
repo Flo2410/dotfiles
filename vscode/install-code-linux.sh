590cdaf717fc657e060947d0d43636a182b73721
Flo2410 <florian@hye.dev>
2022-10-17 15:46:01 +0200

    Ubuntu

---
+++ vscode/install-code-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$(dirname "$(realpath $0)")
+echo "Installing vs-code settings..."
+cd $HOME
+
+echo "Linking settings.json..."
+ln -sf $HOME/dotfiles/vscode/settings.json $HOME/.config/Code/User/
+
+echo "Linking keybindings.json..."
+ln -sf $HOME/dotfiles/vscode/keybindings.json $HOME/.config/Code/User/
+
+echo "Linking snippets..."
+ln -sf $HOME/dotfiles/vscode/snippets/* $HOME/.config/Code/User/snippets/
