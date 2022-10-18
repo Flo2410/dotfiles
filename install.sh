70d351024a613e1c15cd74b142aad96462c66927
Florian Hye <florian@hye.dev>
2022-10-18 11:01:22 +0200

    added check for Linux

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/zsh
 echo "Starting full install..."
+
 ./zsh/install-zsh.sh
-./vscode/install-code.sh
+
+if [ $(uname -s) = "Linux" ]; then
+  echo "Linux"
+  ./vscode/install-code-linux.sh
+else 
+  echo "Darwin"
+  ./vscode/install-code.sh
+ fi
+ 
