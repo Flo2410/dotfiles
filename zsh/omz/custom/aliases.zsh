bae23656948187fbd8f86be9866d1feeac446992
Flo2410 <florian@hye.dev>
2023-02-10 18:05:56 +0100

    made Syncthing path lowercase

--- zsh/omz/custom/aliases.zsh
+++ zsh/omz/custom/aliases.zsh
@@ -1,24 +1,24 @@
 # go into the programming folder
-alias go.prg="cd ~/Syncthing/Programmieren"
-alias go.node="cd ~/Syncthing/Programmieren/Node"
-alias go.web="cd ~/Syncthing/Programmieren/Web"
-alias go.react="cd ~/Syncthing/Programmieren/React"
-alias go.dpl="cd ~/Syncthing/Programmieren/Node/dpl_v4"
-alias go.esp="cd ~/Syncthing/Programmieren/esp"
+alias go.prg="cd ~/syncthing/Programmieren"
+alias go.node="cd ~/syncthing/Programmieren/Node"
+alias go.web="cd ~/syncthing/Programmieren/Web"
+alias go.react="cd ~/syncthing/Programmieren/React"
+alias go.dpl="cd ~/syncthing/Programmieren/Node/dpl_v4"
+alias go.esp="cd ~/syncthing/Programmieren/esp"
 
 # go into fhwn folders
-alias go.fhwnRoot="cd ~/Syncthing/Dokumente/Schule/FHWN/"
-alias go.fhwnS1="cd ~/Syncthing/Dokumente/Schule/FHWN/1_Semester_W2020"
-alias go.fhwnS2="cd ~/Syncthing/Dokumente/Schule/FHWN/2_Semester_S2021"
-alias go.fhwnS3="cd ~/Syncthing/Dokumente/Schule/FHWN/3_Semester_W2021"
-alias go.fhwnS4="cd ~/Syncthing/Dokumente/Schule/FHWN/4_Semester_S2022"
-alias go.fhwnS5="cd ~/Syncthing/Dokumente/Schule/FHWN/5_Semester_W2022"
-alias go.fhwnS6="cd ~/Syncthing/Dokumente/Schule/FHWN/6_Semester_S2023"
+alias go.fhwnRoot="cd ~/syncthing/Dokumente/Schule/FHWN/"
+alias go.fhwnS1="cd ~/syncthing/Dokumente/Schule/FHWN/1_Semester_W2020"
+alias go.fhwnS2="cd ~/syncthing/Dokumente/Schule/FHWN/2_Semester_S2021"
+alias go.fhwnS3="cd ~/syncthing/Dokumente/Schule/FHWN/3_Semester_W2021"
+alias go.fhwnS4="cd ~/syncthing/Dokumente/Schule/FHWN/4_Semester_S2022"
+alias go.fhwnS5="cd ~/syncthing/Dokumente/Schule/FHWN/5_Semester_W2022"
+alias go.fhwnS6="cd ~/syncthing/Dokumente/Schule/FHWN/6_Semester_S2023"
 alias go.fhwn="go.fhwnS6"
-alias go.bac="cd ~/Syncthing/Dokumente/Schule/FHWN/6_Semester_S2023/BAC"
+alias go.bac="cd ~/syncthing/Dokumente/Schule/FHWN/6_Semester_S2023/BAC"
 
 # go into work dirs
-alias go.expanic="cd ~/Syncthing/Dokumente/Arbeit/expanic"
+alias go.expanic="cd ~/syncthing/Dokumente/Arbeit/expanic"
 
 # load esp-idf
 alias load.idf=". $HOME/esp/esp-idf/export.sh"
