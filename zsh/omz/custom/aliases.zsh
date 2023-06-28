40a1ff055ae31fe7d43fd5e6e622bfb55bb037fd
Flo2410 <florian@hye.dev>
2023-06-28 12:45:55 +0200

    changed some aliases

--- zsh/omz/custom/aliases.zsh
+++ zsh/omz/custom/aliases.zsh
@@ -8,17 +8,8 @@ alias go.esp="cd ~/syncthing/Development/esp"
 
 # go into fhwn folders
 alias go.fhwnRoot="cd ~/syncthing/Dokumente/Schule/FHWN/"
-alias go.fhwnS1="cd ~/syncthing/Dokumente/Schule/FHWN/1_Semester_W2020"
-alias go.fhwnS2="cd ~/syncthing/Dokumente/Schule/FHWN/2_Semester_S2021"
-alias go.fhwnS3="cd ~/syncthing/Dokumente/Schule/FHWN/3_Semester_W2021"
-alias go.fhwnS4="cd ~/syncthing/Dokumente/Schule/FHWN/4_Semester_S2022"
-alias go.fhwnS5="cd ~/syncthing/Dokumente/Schule/FHWN/5_Semester_W2022"
-alias go.fhwnS6="cd ~/syncthing/Dokumente/Schule/FHWN/6_Semester_S2023"
-alias go.fhwn="go.fhwnS6"
-alias go.bac="cd ~/syncthing/Dokumente/Schule/FHWN/6_Semester_S2023/BAC"
-
-# go into work dirs
-alias go.expanic="cd ~/syncthing/Dokumente/Arbeit/expanic"
+alias go.fhwn="go.fhwnRoot"
+alias go.bac="cd ~/syncthing/Dokumente/Schule/FHWN/BRO/6_Semester_S2023/BAC"
 
 # load esp-idf
 alias load.idf=". $HOME/esp/esp-idf/export.sh"
@@ -29,6 +20,9 @@ alias settings.conda.hide_env="conda config --set changeps1 false"
 
 # Find & Delete all ".DS_Store" files (recursive)
 alias delete.ds="find . -name '.DS_Store' -type f -print -delete"
+
+# mounts
+alias mount.florian="sudo mount -t cifs  //sagittarius-a.milkyway/florian /mnt/florian -o user=florian"
 
 alias zsh.reload=". ~/.zshrc"
 
